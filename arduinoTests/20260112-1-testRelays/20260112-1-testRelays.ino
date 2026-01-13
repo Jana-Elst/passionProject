7eed2c47d5b098aa5d342a5a905b287db7e889f8
Jana <jana.elst@student.howest.be>
2026-01-13 09:58:13 +0100

    added a arduino script, changed the readme file, changed typo in blog

---
+++ arduinoTests/20260112-1-testRelays/20260112-1-testRelays.ino
@@ -0,0 +1,26 @@
+// Define the pin connected to the WPM406 signal (S) pin
+// Based on your board file, this is Digital Pin 3
+const int RELAY_PIN1 = 7;
+const int RELAY_PIN2 = 8; 
+
+void setup() {
+  // Set the relay pin as an OUTPUT
+  pinMode(RELAY_PIN1, OUTPUT);
+    pinMode(RELAY_PIN2, OUTPUT);
+
+  
+  // Start with the relay OFF (NC connected to C)
+  digitalWrite(RELAY_PIN1, LOW);
+    digitalWrite(RELAY_PIN2, LOW);
+
+  
+  // Optional: Start Serial for debugging
+  Serial.begin(9600);
+  Serial.println("Relay Test Starting...");
+}
+
+void loop() {
+  // Turn the relay ON (Energized)
+  // C (Common) connects to NO (Normally Open)
+  Serial.println("Relay ON (NO Active)");
+}
