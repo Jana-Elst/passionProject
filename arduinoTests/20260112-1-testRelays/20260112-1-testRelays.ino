af7e0edaf35489b3adcbce806d1d195a4b9a2ab6
Jana <jana.elst@student.howest.be>
2026-01-22 10:03:51 +0100

    first python script

--- arduinoTests/20260112-1-testRelays/20260112-1-testRelays.ino
+++ arduinoTests/20260112-1-testRelays/20260112-1-testRelays.ino
@@ -1,18 +1,13 @@
 // Define the pin connected to the WPM406 signal (S) pin
 // Based on your board file, this is Digital Pin 3
-const int RELAY_PIN1 = 7;
-const int RELAY_PIN2 = 8; 
+const int RELAY_PIN1 = 10;
 
 void setup() {
   // Set the relay pin as an OUTPUT
   pinMode(RELAY_PIN1, OUTPUT);
-    pinMode(RELAY_PIN2, OUTPUT);
-
   
   // Start with the relay OFF (NC connected to C)
   digitalWrite(RELAY_PIN1, HIGH);
-    digitalWrite(RELAY_PIN2, HIGH);
-
   
   // Optional: Start Serial for debugging
   Serial.begin(9600);
