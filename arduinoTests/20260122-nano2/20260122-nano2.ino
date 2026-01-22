38892facc8fbf5c360959d221131794baabdff83
Jana <jana.elst@student.howest.be>
2026-01-22 17:29:06 +0100

    added handshakes for nanos

---
+++ arduinoTests/20260122-nano2/20260122-nano2.ino
@@ -0,0 +1,30 @@
+//--- CONFIG
+const String DeviceName = "T2";
+
+//------------------------ SETUP ------------------------//
+void setup() { Serial.begin(9600); }
+
+//------------------------ LOOP ------------------------//
+void loop() {
+  //--- 0. Handshake
+  // this should loop till the handshake is done
+  while (!handshake()) {
+    delay(100);
+  }
+}
+
+//------------------------ Give handshake to the PI ------------------------//
+bool handshake() {
+  if (Serial.available() > 0) {
+    String incoming = Serial.readStringUntil('\n');
+    incoming.trim(); // Remove any whitespace/newlines
+
+    // When the Pi asks "IDENTIFY", send back the name
+    if (incoming == "IDENTIFY") {
+      Serial.println(DeviceName);
+      return true;
+    }
+  }
+
+  return false;
+}
