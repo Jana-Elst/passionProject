a2593ba058a10d38c95a170e79100f1daf063f75
Jana <jana.elst@student.howest.be>
2026-02-06 22:23:41 +0100

    change DTMF

--- finalFiles/script.py
+++ finalFiles/script.py
@@ -88,23 +88,27 @@ TONE_RINGBACK        = ("TONE", 400, 0.4, 17, 0.95)
 SILENCE_RINGBACK     = ("PAUSE", 2.0)
 SILENCE_RINGBACK_S   = ("PAUSE", 0.2)
 DIALING_SEQUENCE = [
-    ("DTMF", 770, 1336, 0.2),  # Digit 5
-    ("PAUSE", 0.02),
-    ("DTMF", 852, 1477, 0.2),  # Digit 9
-    ("PAUSE", 0.02),
-    ("DTMF", 770, 1336, 0.2),  # Digit 5
-    ("PAUSE", 0.02),
-    ("DTMF", 941, 1336, 0.2),  # Digit 0
-    ("PAUSE", 0.02),
-    ("DTMF", 697, 1209, 0.2),  # Digit 1
-    ("PAUSE", 0.02),
-    ("DTMF", 770, 1336, 0.2),  # Digit 5
-    ("PAUSE", 0.02), 
-    ("DTMF", 852, 1477, 0.2),  # Digit 9
-    ("PAUSE", 0.02),
-    ("DTMF", 770, 1477, 0.2),  # Digit 6
-    ("PAUSE", 0.02),
-    ("DTMF", 1633, 941, 0.2),  # Digit D
+    ("DTMF", 770, 1336, 0.1),  # Digit 5
+    ("PAUSE", 0.01),
+    ("DTMF", 852, 1477, 0.1),  # Digit 9
+    ("PAUSE", 0.01),
+    ("DTMF", 770, 1336, 0.1),  # Digit 5
+    ("PAUSE", 0.01),
+    ("DTMF", 941, 1336, 0.1),  # Digit 0
+    ("PAUSE", 0.01),
+    ("DTMF", 697, 1209, 0.1),  # Digit 1
+    ("PAUSE", 0.01),
+    ("DTMF", 770, 1336, 0.1),  # Digit 5
+    ("PAUSE", 0.01), 
+    ("DTMF", 852, 1477, 0.1),  # Digit 9
+    ("PAUSE", 0.01),
+    ("DTMF", 770, 1477, 0.1),  # Digit 6
+    ("PAUSE", 0.01),
+    ("DTMF", 941, 1633, 0.1),  # Digit D
+    ("PAUSE", 0.01),
+    ("DTMF", 941, 1633, 0.1),  # Digit D
+    ("PAUSE", 0.01),
+    ("DTMF", 941, 1633, 0.1),  # Digit D
 ]
 CONNECTING_TONE = DIALING_SEQUENCE
 
